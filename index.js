fadf137d97cd2ff03beee96b1513bded10d3a71e
bcalou <bastien.calou@gmail.com>
2021-04-30 17:41:02 +0200

    Check if the outputPath is valid (#3)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const critical = require("critical");
 
 module.exports = function (config, options) {
   config.addTransform("critical-css", async function (content, outputPath) {
-    if (outputPath.endsWith(".html")) {
+    if (outputPath && outputPath.endsWith(".html")) {
       const { html } = await critical.generate({
         assetPaths: [path.dirname(outputPath)],
         base: this._config.dir.output,
