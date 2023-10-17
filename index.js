364f1800063c1c3daed999771661d7fc969035d0
Greg Ives <greg@gregives.co.uk>
2023-10-17 01:52:31 +0100

    Update dependencies

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const path = require("path");
-const critical = require("critical");
 
 module.exports = function (config, options) {
+  let critical;
+
+  config.on("eleventy.before", async () => {
+    critical = await import("critical");
+  });
+
   config.addTransform("critical-css", async function (content, outputPath) {
     if (outputPath && outputPath.endsWith(".html")) {
-      // Config will change from Eleventy v1.0.0
-      const outputDir =
-        (this._config && this._config.dir && this._config.dir.output) ||
-        (config && config.dir && config.dir.output) ||
-        path.dirname(outputPath);
-
-      // Generate HTML with critical CSS
       const { html } = await critical.generate({
         assetPaths: [path.dirname(outputPath)],
-        base: outputDir,
+        base: config.dir.output,
         html: content,
         inline: true,
         rebase: ({ originalUrl }) => originalUrl,
